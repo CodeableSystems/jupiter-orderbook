764a072c8e62923b0c66d8992d183a93bd0e746e
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2023-08-21 19:25:07 +0200

    Add example scripts

---
+++ scripts/createSellOrder.js
@@ -0,0 +1,69 @@
+import { Connection, Keypair, VersionedTransaction } from '@solana/web3.js';
+import fetch from 'cross-fetch';
+import { Wallet } from '@project-serum/anchor';
+import bs58 from 'bs58';
+
+// Use your own RPC endpoint. This one points to the Solana mainnet cluster and cannot be used for production.
+const connection = new Connection('https://api.mainnet-beta.solana.com');
+
+let wallets=[
+  // your private keys here, two wallets. Referrals are optional, but you can get referral fees if you provide them
+]
+
+const wallet = new Wallet(Keypair.fromSecretKey(bs58.decode(wallets[0])));
+const referral = new Wallet(Keypair.fromSecretKey(bs58.decode(wallets[1])));
+
+const inputMint = "5qKDWkBejLtRh1UGFV7e58QEkdn2fRyH5ehVXqUYujNW";//SCOIN
+const outputMint = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";//USDC
+
+// Base key are used to generate a unique order id
+const base = Keypair.generate();
+
+const getCurrentPrice = await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=${outputMint}&outputMint=${inputMint}&amount=1000000&slippageBps=50&onlyDirectRoutes=false&experimentalDexes=Jupiter LO`)
+const currentPrice = await getCurrentPrice.json()
+
+const scoinAmount = 200000;
+
+// get serialized transactions
+const transactions = await (
+  await fetch('https://jup.ag/api/limit/v1/createOrder', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      owner: wallet.publicKey.toString(),
+      inAmount: scoinAmount * 1e9,//currentPrice.inAmount*scoinAmount,
+      outAmount: scoinAmount * 1220,
+      inputMint: inputMint.toString(),
+      outputMint: outputMint.toString(),
+      expiredAt: null,// or add an expiration date in unix timestamp format
+      base: base.publicKey.toString(),
+      // referralAccount and name are both optional
+      // provide both to get referral fees
+      //referralAccount: referral.publicKey.toString(),
+      //referralName: "your name"
+    })
+  })
+).json();
+
+const { tx } = transactions;
+// deserialize the transaction
+const transactionBuf = Buffer.from(tx, 'base64');
+var transaction = VersionedTransaction.deserialize(transactionBuf);
+
+// sign the transaction using the required key
+// for create order, wallet and base key are required.
+transaction.sign([wallet.payer, base]);
+
+// Execute the transaction
+const rawTransaction = transaction.serialize()
+const txid = await connection.sendRawTransaction(rawTransaction, {
+  skipPreflight: true,
+  maxRetries: 2
+});
+
+await connection.confirmTransaction(txid);
+
+console.log(`https://solscan.io/tx/${txid}`);
+
